834126a7c9139e870f9d6dd5350366164377603f
DaniSoftville <danisoftville@gmail.com>
2024-03-10 10:01:06 +0000

    WebApi created and cleaning the template

---
+++ API/Program.cs
@@ -0,0 +1,27 @@
+/* This program.cs class is the starting point of our app */
+
+var builder = WebApplication.CreateBuilder(args);
+
+/* Add services to the container.(Dependency Injection Container) when we wanna use a particular service inside of our classes in our project, 
+then we're gonna use the facility called dependency injection to inject that service to be available to be used inside our app. */
+
+builder.Services.AddControllers(); /* Because we're using a web api, we use this service api controller. 
+We add our controllers to our project inside this container */
+builder.Services.AddEndpointsApiExplorer(); //We need these coiuple of services to generate the swagger content we see in the browser
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+/*  Configure the HTTP request pipeline.We can add middleware inside this pipeline against that request,
+ and do something with this request */
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers(); //The API knows where to send the requests because we add a route configuration for ou controllers here
+
+app.Run();
